ba7608160c5e4ceed6e8782e902d91c09bee345d
fengcy-liu <1090903525@qq.com>
2021-07-17 11:07:49 +0800

    add 2048 Demo and fisheating Demo

---
+++ Code/fishEating/src/fishEating.cpp
@@ -0,0 +1,332 @@
+﻿// fishEating.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <graphics.h>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
+#include <MMSystem.h>
+#include "Vfw.h"
+
+#pragma comment (lib, "Vfw32.lib")
+
+#pragma comment(lib,"Winmm.lib")
+
+#define WINDOW_WIDTH                (1000)
+#define WINDOW_LENGTH               (640)
+#define WINDOW_GAP                  (40)
+#define WINDOW_BLOCK_SIZE           (80)
+
+#define CLASS_NUM_OF_FISH                 (5)
+#define CLASS_NUM_OF_TO0L                 (2)
+
+#define IMG_SRC_LEFT_IDX            (2)
+#define IMG_SRC_RIGHT_IDX           (0)
+#define TOOL_IDX_OFFSET             (CLASS_NUM_OF_FISH)
+
+#define MOVE_RIGHT                  (0)
+#define MOVE_LEFT                   (1)
+#define FISH_START_OFFSET           (200)
+
+#define CYCLE_MS                    (16)
+
+using namespace std;
+
+void playMv(void);
+
+int a = (SRCINVERT, NOTSRCERASE);
+IMAGE img_bk; 
+IMAGE img_enter;
+IMAGE img_player[4];
+IMAGE img_nfood[5][4];
+IMAGE img_sfood[2][4];
+
+typedef struct
+{
+    int width;//宽度
+    int height;//高度
+}fishImgInfo_t;
+
+fishImgInfo_t fishImgInfo[CLASS_NUM_OF_FISH + CLASS_NUM_OF_TO0L] =
+{
+    {30, 20},
+    {40, 25},
+    {60, 45},
+    {110, 60},
+    {160, 70},
+    {30, 30},
+    {30, 30},
+};
+
+#define NUM_OF_BLOCK               ((WINDOW_LENGTH- 2 * WINDOW_GAP) / WINDOW_BLOCK_SIZE)
+
+int blockDir[NUM_OF_BLOCK];
+int blockFoodCnt[NUM_OF_BLOCK];
+
+typedef struct
+{
+    int size;//大小
+    int vx;//x轴速度
+    int vy;//y轴速度
+}fish_t;
+
+fish_t fishInfo[CLASS_NUM_OF_FISH + CLASS_NUM_OF_TO0L + 1] = {
+    {0, 4, 0},
+    {0, 7, 0},
+    {0, 6, 0},
+    {0, 6, 0},
+    {0, 5, 0},
+    {0, 2, 0},
+    {0, 2, 0},
+    {0, 10, 20},
+};
+
+typedef struct
+{
+    int id;
+    int posx;//x坐标
+    int posy;//y坐标
+    int dir;//朝向
+    int blockSeq;
+}food_t;
+
+typedef struct foodListTag
+{
+    food_t food;
+    struct foodListTag* next;
+}foodList_t;
+
+foodList_t *foodHead;
+
+void DrawFood(IMAGE* pFood, int x, int y)
+{
+    putimage(x, y, &pFood[0], NOTSRCERASE);
+    putimage(x, y, &pFood[1], SRCINVERT);
+}
+
+void resizePlayer(int x, int y)
+{
+
+    loadimage(&img_player[0], _T("./source/playerbei.jpg"), x, y, TRUE);
+    loadimage(&img_player[1], _T("./source/player.jpg"), x, y, TRUE);
+    loadimage(&img_player[2], _T("./source/f_playerbei.jpg"), x, y, TRUE);
+    loadimage(&img_player[3], _T("./source/f_player.jpg"), x, y, TRUE);
+}
+
+void Init()
+{
+    //加载背景图
+    loadimage(&img_bk,     _T("./source/background.jpg"), WINDOW_WIDTH, WINDOW_LENGTH, TRUE);
+    loadimage(&img_enter,  _T("./source/enter.jpg"), WINDOW_WIDTH, WINDOW_LENGTH, TRUE);
+
+    //加载玩家图
+    loadimage(&img_player[0], _T("./source/playerbei.jpg"), 35, 25, TRUE);
+    loadimage(&img_player[1], _T("./source/player.jpg"), 35, 25, TRUE);
+    loadimage(&img_player[2], _T("./source/f_playerbei.jpg"), 35, 25, TRUE);
+    loadimage(&img_player[3], _T("./source/f_player.jpg"), 35, 25, TRUE);
+
+    //加载食物图
+    loadimage(&img_nfood[0][0], _T("./source/food1bei.jpg"), fishImgInfo[0].width, fishImgInfo[0].height, TRUE);
+    loadimage(&img_nfood[0][1], _T("./source/food1.jpg"), fishImgInfo[0].width, fishImgInfo[0].height, TRUE);
+    loadimage(&img_nfood[0][2], _T("./source/f_food1bei.jpg"), fishImgInfo[0].width, fishImgInfo[0].height, TRUE);
+    loadimage(&img_nfood[0][3], _T("./source/f_food1.jpg"), fishImgInfo[0].width, fishImgInfo[0].height, TRUE);
+
+    loadimage(&img_nfood[1][0], _T("./source/food2bei.jpg"), fishImgInfo[1].width, fishImgInfo[1].height, TRUE);
+    loadimage(&img_nfood[1][1], _T("./source/food2.jpg"), fishImgInfo[1].width, fishImgInfo[1].height, TRUE);
+    loadimage(&img_nfood[1][2], _T("./source/f_food2bei.jpg"), fishImgInfo[1].width, fishImgInfo[1].height, TRUE);
+    loadimage(&img_nfood[1][3], _T("./source/f_food2.jpg"), fishImgInfo[1].width, fishImgInfo[1].height, TRUE);
+
+    loadimage(&img_nfood[2][0], _T("./source/food3bei.jpg"), fishImgInfo[2].width, fishImgInfo[2].height, TRUE);
+    loadimage(&img_nfood[2][1], _T("./source/food3.jpg"), fishImgInfo[2].width, fishImgInfo[2].height, TRUE);
+    loadimage(&img_nfood[2][2], _T("./source/f_food3bei.jpg"), fishImgInfo[2].width, fishImgInfo[2].height, TRUE);
+    loadimage(&img_nfood[2][3], _T("./source/f_food3.jpg"), fishImgInfo[2].width, fishImgInfo[2].height, TRUE);
+
+    loadimage(&img_nfood[3][0], _T("./source/food4bei.jpg"), fishImgInfo[3].width, fishImgInfo[3].height, TRUE);
+    loadimage(&img_nfood[3][1], _T("./source/food4.jpg"), fishImgInfo[3].width, fishImgInfo[3].height, TRUE);
+    loadimage(&img_nfood[3][2], _T("./source/f_food4bei.jpg"), fishImgInfo[3].width, fishImgInfo[3].height, TRUE);
+    loadimage(&img_nfood[3][3], _T("./source/f_food4.jpg"), fishImgInfo[3].width, fishImgInfo[3].height, TRUE);
+
+    loadimage(&img_nfood[4][0], _T("./source/food5bei.jpg"), fishImgInfo[4].width, fishImgInfo[4].height, TRUE);
+    loadimage(&img_nfood[4][1], _T("./source/food5.jpg"), fishImgInfo[4].width, fishImgInfo[4].height, TRUE);
+    loadimage(&img_nfood[4][2], _T("./source/f_food5bei.jpg"), fishImgInfo[4].width, fishImgInfo[4].height, TRUE);
+    loadimage(&img_nfood[4][3], _T("./source/f_food5.jpg"), fishImgInfo[4].width, fishImgInfo[4].height, TRUE);
+
+    loadimage(&img_sfood[0][0], _T("./source/timebei.jpg"), fishImgInfo[5].width, fishImgInfo[5].height, TRUE);
+    loadimage(&img_sfood[0][1], _T("./source/time.jpg"), fishImgInfo[5].width, fishImgInfo[5].height, TRUE);
+
+    loadimage(&img_sfood[1][0], _T("./source/zhadan1bei.jpg"), fishImgInfo[6].width, fishImgInfo[6].height, TRUE);
+    loadimage(&img_sfood[1][1], _T("./source/zhadan1.jpg"), fishImgInfo[6].width, fishImgInfo[6].height, TRUE);
+
+
+    initgraph(WINDOW_WIDTH, WINDOW_LENGTH);
+    putimage(0, 0, &img_bk, SRCCOPY);
+
+    /* 初始化背景音乐 */
+    mciSendString(_T("open ./source/music.mp3 alias bkmusic"), NULL, 0, NULL);//打开背景音乐
+    mciSendString(_T("play bkmusic repeat"), NULL, 0, NULL);  // 循环播放
+
+    //playMv();
+    foodHead = new foodList_t;
+    memset(foodHead, 0, sizeof(foodList_t));
+
+}
+
+int getFoodListSize(void)
+{
+    int cnt = 0;
+    foodList_t* pFood = foodHead;
+    while (pFood->next != NULL)
+    {
+        pFood = pFood->next;
+        cnt++;
+    }
+    return cnt;
+}
+
+void show(void)
+{
+    foodList_t* pFood = foodHead;
+    // 初始化背景
+    putimage(0, 0, &img_bk, SRCCOPY);
+    //画鱼
+    while (pFood->next != NULL)
+    {
+        pFood = pFood->next;
+        if (pFood->food.id < CLASS_NUM_OF_FISH)
+        {
+            if (pFood->food.dir == 1)
+            {
+                DrawFood(&img_nfood[pFood->food.id][IMG_SRC_RIGHT_IDX], pFood->food.posx, pFood->food.posy);
+            }
+            else
+            {
+                DrawFood(&img_nfood[pFood->food.id][IMG_SRC_LEFT_IDX], pFood->food.posx, pFood->food.posy);
+            }
+        }
+    }
+}
+
+void updateFoodList(void)
+{
+    foodList_t* pFood = foodHead;
+    int id, dir;
+    while (pFood->next != NULL)
+    {
+        pFood = pFood->next;
+        id = pFood->food.id;
+        dir = pFood->food.dir;
+        pFood->food.posx += fishInfo[id].vx * dir;
+    }
+}
+
+void createNewFood(int blockSeq)
+{
+    foodList_t* pnew = new foodList_t;
+    memset(pnew, 0, sizeof(foodList_t));
+
+    pnew->food.id = rand()%CLASS_NUM_OF_FISH;
+    pnew->food.blockSeq = blockSeq;
+    if (blockDir[blockSeq] == MOVE_RIGHT)
+    {
+        pnew->food.posx = -FISH_START_OFFSET;
+        pnew->food.dir = 1;
+    }
+    else
+    {
+        pnew->food.posx = (WINDOW_WIDTH + FISH_START_OFFSET);
+        pnew->food.dir = -1;
+    }
+    pnew->food.posy = WINDOW_GAP + blockSeq * WINDOW_BLOCK_SIZE;
+    pnew->next = foodHead->next;
+    foodHead->next = pnew;
+    blockFoodCnt[blockSeq]++;
+}
+
+void deleteBoundryFood(void)
+{
+    foodList_t* pFood = foodHead;
+    while (pFood && pFood->next != NULL)
+    {
+        foodList_t* temp = pFood->next;
+        if (temp->food.posx < -FISH_START_OFFSET || temp->food.posx > (WINDOW_WIDTH + FISH_START_OFFSET))
+        {
+            pFood->next = pFood->next->next;
+            blockFoodCnt[temp->food.blockSeq]--;
+            delete temp;
+        }
+        pFood = pFood->next;
+    }
+
+}
+
+void playMusic(bool status)
+{
+    mciSendString(_T("stop gemusic"), NULL, 0, NULL);   // 先把前面一次的音乐停止
+    mciSendString(_T("close gemusic"), NULL, 0, NULL); // 先把前面一次的音乐关闭
+    if (status == TRUE)
+    {
+        mciSendString(_T("open ./source/1.mp3 alias gemusic"), NULL, 0, NULL); // 打开跳动音乐
+    }
+    else
+    {
+        mciSendString(_T("open ./source/2.mp3 alias gemusic"), NULL, 0, NULL); // 打开跳动音乐
+    }
+    mciSendString(_T("play gemusic"), NULL, 0, NULL); // 仅播放一次
+}
+
+void playMv(void)
+{
+    HWND hwnd = MCIWndCreate(GetHWnd(), NULL, WS_CHILD | WS_VISIBLE | MCIWNDF_NOMENU | MCIWNDF_NOPLAYBAR, NULL);
+    SetWindowPos(hwnd, HWND_TOP, 100, 100, 256, 256, SWP_SHOWWINDOW);
+    MCIWndOpen(hwnd, _T("C:\\FFOutput\\jade.wmv"), NULL);
+    int length = MCIWndGetLength(hwnd);
+    MCIWndPlay(hwnd);
+    while (TRUE)
+    {
+        if (_kbhit())
+        {
+            break;
+        }
+    }
+    MCIWndClose(hwnd);
+}
+
+void game(void)
+{
+    bool exit = FALSE;
+    static clock_t start_ms, finish_ms;
+    start_ms = clock();
+    BeginBatchDraw();
+    srand(static_cast<unsigned int>(time(NULL)));
+    while (!exit)
+    {
+        for (int i = 0; i < NUM_OF_BLOCK; i++)
+        {
+            int rm = rand() % 10;
+            if (blockFoodCnt[i] == 0 && rm < 7)
+            {
+                createNewFood(i);
+            }
+        }
+        updateFoodList();
+        deleteBoundryFood();
+        show();
+        FlushBatchDraw();
+        do
+        {
+            finish_ms = clock();
+        } while ((finish_ms - start_ms) < CYCLE_MS);
+        start_ms = finish_ms;
+    }
+    EndBatchDraw();
+}
+
+int main()
+{
+    Init();
+    //game();
+    _getch();
+    closegraph();
+}
+
---
+++ Code/game2048/game2048.cpp
@@ -0,0 +1,597 @@
+﻿// game2048.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <Windows.h>
+#include <time.h>
+#include <graphics.h>
+
+using namespace std;
+
+#define WIDTH               (720)
+#define HEIGHT              (720)
+#define SQUARE_LENGTH       (80)
+#define SQUARE_GAP          (15)
+
+#define NUM_OF_BLOCK         (4)
+
+#define MAX_GAME_EXPONENT               (11)
+//#define PLAYER_TARGET                   (2048)
+
+#define MOVING_LEFT          (1)
+#define MOVING_RIGHT         (2)
+#define MOVING_UP            (3)
+#define MOVING_DOWN          (4)
+
+#define GENERATE2_PROBABILITY   (95) // 95% generate 2, 5% generate 4;
+
+#ifndef TRUE
+#define TRUE
+#endif
+#ifndef FALSE
+#define FALSE
+#endif
+
+
+void  createNewBlock(void);
+bool  keyControl(void);
+void  drawSquare(int x, int y, int length);
+float dynTextSizeRatio(int val);
+void  drawBlock(void);
+void  showVal(void);
+bool  combineVal(int mode);
+void  gameInit(void);
+
+int blockVal[NUM_OF_BLOCK][NUM_OF_BLOCK];
+int blockPos[NUM_OF_BLOCK][2];
+RECT blockRect[NUM_OF_BLOCK][NUM_OF_BLOCK];
+
+/* 存储空余区块 */
+int* pEmptyBlock[NUM_OF_BLOCK * NUM_OF_BLOCK];
+/* 记录空余区块 */
+int emptyBlock;
+
+bool bFull = FALSE;
+bool bTarget = FALSE;
+
+const COLORREF color[MAX_GAME_EXPONENT + 1] = {
+
+    LIGHTGRAY,
+    CYAN,
+    LIGHTCYAN,
+    BROWN,
+    BLUE,
+    LIGHTBLUE,
+    GREEN,
+    LIGHTGREEN,
+    MAGENTA,
+    LIGHTMAGENTA,
+    RED,
+    LIGHTRED,
+};
+
+const LPCTSTR displayNum[MAX_GAME_EXPONENT + 1] = {
+
+    _T("0"),
+    _T("2"),
+    _T("4"),
+    _T("8"),
+    _T("16"),
+    _T("32"),
+    _T("64"),
+    _T("128"),
+    _T("256"),
+    _T("512"),
+    _T("1024"),
+    _T("2048"),
+};
+
+void drawSquare(int x, int y, int length)
+{
+    int leftx = x;
+    int topy = y;
+    int rightx = x + length;
+    int bottomy = y + length;
+    //fillrectangle(leftx, topy, rightx, bottomy);
+    fillroundrect(leftx, topy, rightx, bottomy, 20, 20);
+}
+
+/* 动态显示字体大小 */
+float dynTextSizeRatio(int val)
+{
+    float ratio;
+    // 1位最大数, 2 ^ 3 = 8
+    if (val < 4)
+    {
+        ratio = 0.9f;
+    }
+    else if (val < 7)// 2位最大数, 2 ^ 6 = 64
+    {
+        ratio = 0.8f;
+    }
+    else if (val < 10)// 3位最大数, 2 ^ 9 = 512
+    {
+        ratio = 0.7f;
+    }
+    else
+    {
+        ratio = 0.5f;
+    }
+    return ratio;
+}
+
+void drawBlock(void)
+{
+    int i, j;
+    /* 画区块 */
+    for (i = 0; i < NUM_OF_BLOCK; i++)
+    {
+        for (j = 0; j < NUM_OF_BLOCK; j++)
+        {
+            setfillcolor(color[blockVal[i][j]]);
+            drawSquare(blockPos[j][0], blockPos[i][1], SQUARE_LENGTH);
+        }
+    }
+}
+
+void showVal(void)
+{
+    int i, j;
+    LOGFONT prefont;
+    int preMode;
+    int nHeight = SQUARE_LENGTH - SQUARE_GAP * 1;
+
+    /* 记录之前配置 */
+    gettextstyle(&prefont);
+    preMode = getbkmode();
+    /* 设置背景透明色 */
+    setbkmode(TRANSPARENT);
+
+    for (i = 0; i < NUM_OF_BLOCK; i++)
+    {
+        for (j = 0; j < NUM_OF_BLOCK; j++)
+        {
+            if (blockVal[i][j] != 0)
+            {
+                settextstyle((int)((float)nHeight * dynTextSizeRatio(blockVal[i][j])), 0, _T("黑体"));
+                drawtext(displayNum[blockVal[i][j]], &blockRect[i][j], DT_SINGLELINE | DT_VCENTER | DT_CENTER);
+            }
+
+            if (blockVal[i][j] == MAX_GAME_EXPONENT)
+            {
+                bTarget = TRUE;
+            }
+        }
+    }
+
+    /* 还原之前配置 */
+    setbkmode(preMode);
+    settextstyle(&prefont);
+}
+
+void SetEmptyBlock(void)
+{
+    int i, j;
+
+    emptyBlock = 0;
+    for (i = 0; i < NUM_OF_BLOCK; i++)
+    {
+        for (j = 0; j < NUM_OF_BLOCK; j++)
+        {
+            if (blockVal[i][j] == 0)
+            {
+                pEmptyBlock[emptyBlock++] = &blockVal[i][j];
+            }
+        }
+    }
+
+    if (emptyBlock == 0)
+    {
+        bFull = TRUE;
+    }
+}
+
+void createOneNewBlock(void)
+{
+    int i1;
+
+    srand((unsigned int)time(NULL));
+
+    if (emptyBlock >= 1)
+    {
+        /* 生成1个随机数 */
+        i1 = rand() % emptyBlock;
+        int gen2 = rand() % 100;
+        if (gen2 < GENERATE2_PROBABILITY)
+        {
+            *pEmptyBlock[i1] = 1;
+        }
+        else
+        {
+            *pEmptyBlock[i1] = 2;
+        }
+        emptyBlock -= 1;
+    }
+}
+
+void createTwoNewBlock(void)
+{
+    if (emptyBlock >= 2)
+    {
+        /* 生成2个随机数 */
+
+        vector<int> index(emptyBlock, 0);
+        for (int i = 0; i < emptyBlock; i++)
+        {
+            index[i] = i;
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            int tmp = rand() % emptyBlock;
+            int gen2 = rand() % 100;
+            if (gen2 < GENERATE2_PROBABILITY)
+            {
+                *pEmptyBlock[index[tmp]] = 1;
+            }
+            else
+            {
+                *pEmptyBlock[index[tmp]] = 2;
+            }
+            index[tmp] = emptyBlock;
+            emptyBlock--;
+        }
+#if 0
+        int i1, i2;
+        i1 = rand() % emptyBlock;
+        i2 = rand() % emptyBlock;
+
+        if (i1 == i2)
+        {
+            if (i1 < (emptyBlock - 1))
+            {
+                i2 = i1 + 1;
+            }
+            else
+            {
+                i2 = 0;
+            }
+        }
+
+        *pEmptyBlock[i1] = 1;
+        *pEmptyBlock[i2] = 1;
+
+        emptyBlock -= 2;
+#endif
+    }
+}
+
+
+bool combineVal(int mode)
+{
+    bool ret = TRUE;
+    int i, j;
+
+    switch (mode)
+    {
+    case MOVING_LEFT:
+        /* 合并 */
+        for (i = 0; i < NUM_OF_BLOCK; i++)
+        {
+            for (j = 0; j < NUM_OF_BLOCK; j++)
+            {
+                if (blockVal[i][j] != 0)
+                {
+                    int k = j + 1;
+                    /* 寻找下一个不等于0的值 */
+                    while (k < NUM_OF_BLOCK && blockVal[i][k] == 0)
+                    {
+                        k++;
+                    };
+
+                    if (k < NUM_OF_BLOCK && blockVal[i][j] == blockVal[i][k])
+                    {
+                        blockVal[i][j] += 1;
+                        blockVal[i][k] = 0;
+                    }
+                }
+            }
+        }
+
+        /* 移动 */
+        for (i = 0; i < NUM_OF_BLOCK; i++)
+        {
+            for (j = 0; j < NUM_OF_BLOCK; j++)
+            {
+                if (blockVal[i][j] == 0)
+                {
+                    int k = j + 1;
+                    /* 寻找下一个不等于0的值 */
+                    while (k < NUM_OF_BLOCK && blockVal[i][k] == 0)
+                    {
+                        k++;
+                    };
+
+                    if (k < NUM_OF_BLOCK)
+                    {
+                        blockVal[i][j] = blockVal[i][k];
+                        blockVal[i][k] = 0;
+                    }
+                }
+            }
+        }
+        break;
+
+    case MOVING_RIGHT:
+        /* 合并 */
+        for (i = 0; i < NUM_OF_BLOCK; i++)
+        {
+            for (j = NUM_OF_BLOCK - 1; j > 0; j--)
+            {
+                if (blockVal[i][j] != 0)
+                {
+                    int k = j - 1;
+                    /* 寻找下一个不等于0的值 */
+                    while (k >= 0 && blockVal[i][k] == 0)
+                    {
+                        k--;
+                    };
+
+                    if (k >= 0 && blockVal[i][j] == blockVal[i][k])
+                    {
+                        blockVal[i][j] += 1;
+                        blockVal[i][k] = 0;
+                    }
+                }
+            }
+        }
+
+        /* 移动 */
+        for (i = 0; i < NUM_OF_BLOCK; i++)
+        {
+            for (j = NUM_OF_BLOCK - 1; j > 0; j--)
+            {
+                if (blockVal[i][j] == 0)
+                {
+                    int k = j - 1;
+                    /* 寻找下一个不等于0的值 */
+                    while (k >= 0 && blockVal[i][k] == 0)
+                    {
+                        k--;
+                    };
+
+                    if (k >= 0)
+                    {
+                        blockVal[i][j] = blockVal[i][k];
+                        blockVal[i][k] = 0;
+                    }
+                }
+            }
+        }
+
+        break;
+
+    case MOVING_DOWN:        /* 合并 */
+        for (i = 0; i < NUM_OF_BLOCK; i++)
+        {
+            for (j = NUM_OF_BLOCK - 1; j > 0; j--)
+            {
+                if (blockVal[j][i] != 0)
+                {
+                    int k = j - 1;
+                    /* 寻找下一个不等于0的值 */
+                    while (k >= 0 && blockVal[k][i] == 0)
+                    {
+                        k--;
+                    };
+
+                    if (k >= 0 && blockVal[j][i] == blockVal[k][i])
+                    {
+                        blockVal[j][i] += 1;
+                        blockVal[k][i] = 0;
+                    }
+                }
+            }
+        }
+
+        /* 移动 */
+        for (i = 0; i < NUM_OF_BLOCK; i++)
+        {
+            for (j = NUM_OF_BLOCK - 1; j > 0; j--)
+            {
+                if (blockVal[j][i] == 0)
+                {
+                    int k = j - 1;
+                    /* 寻找下一个不等于0的值 */
+                    while (k >= 0 && blockVal[k][i] == 0)
+                    {
+                        k--;
+                    };
+
+                    if (k >= 0)
+                    {
+                        blockVal[j][i] = blockVal[k][i];
+                        blockVal[k][i] = 0;
+                    }
+                }
+            }
+        }
+        break;
+
+    case MOVING_UP:
+        /* 合并 */
+        for (i = 0; i < NUM_OF_BLOCK; i++)
+        {
+            for (j = 0; j < NUM_OF_BLOCK; j++)
+            {
+                if (blockVal[j][i] != 0)
+                {
+                    int k = j + 1;
+                    /* 寻找下一个不等于0的值 */
+                    while (k < NUM_OF_BLOCK && blockVal[k][i] == 0)
+                    {
+                        k++;
+                    };
+
+                    if (k < NUM_OF_BLOCK && blockVal[j][i] == blockVal[k][i])
+                    {
+                        blockVal[j][i] += 1;
+                        blockVal[k][i] = 0;
+                    }
+                }
+            }
+        }
+
+        /* 移动 */
+        for (i = 0; i < NUM_OF_BLOCK; i++)
+        {
+            for (j = 0; j < NUM_OF_BLOCK; j++)
+            {
+                if (blockVal[j][i] == 0)
+                {
+                    int k = j + 1;
+                    /* 寻找下一个不等于0的值 */
+                    while (k < NUM_OF_BLOCK && blockVal[k][i] == 0)
+                    {
+                        k++;
+                    };
+
+                    if (k < NUM_OF_BLOCK)
+                    {
+                        blockVal[j][i] = blockVal[k][i];
+                        blockVal[k][i] = 0;
+                    }
+                }
+            }
+        }
+        break;
+    default:
+        break;
+    }
+
+    return ret;
+}
+
+bool keyControl(void)
+{
+    bool ret = FALSE;
+    int mode = 0;
+    if (_kbhit())
+    {
+        char key = _getch();
+        switch (key)
+        {
+        case 'W':
+        case 'w':
+            combineVal(MOVING_UP);
+            ret = TRUE;
+            break;
+        case 'A':
+        case 'a':
+            combineVal(MOVING_LEFT);
+            ret = TRUE;
+            break;
+        case 'S':
+        case 's':
+            combineVal(MOVING_DOWN);
+            ret = TRUE;
+            break;
+        case 'D':
+        case 'd':
+            combineVal(MOVING_RIGHT);
+            ret = TRUE;
+            break;
+        default:
+            break;
+        }
+    }
+    return ret;
+}
+
+void gameStart(void)
+{
+
+    BeginBatchDraw();
+    drawBlock();
+    showVal();
+    FlushBatchDraw();
+
+    while (!bTarget && !bFull)
+    {
+        if (keyControl())
+        {
+            SetEmptyBlock();
+            createOneNewBlock();
+            drawBlock();
+            showVal();
+            FlushBatchDraw();
+        }
+    }
+    if (bTarget)
+    {
+        printf("you win!\n");
+    }
+    else if (bFull)
+    {
+        printf("you lose!\n");
+    }
+
+    EndBatchDraw();
+}
+
+void gameInit(void)
+{
+    int i, j;
+    int length;
+    int stx, sty;
+    //initgraph(width, height);
+    initgraph(WIDTH, HEIGHT, EW_SHOWCONSOLE);
+
+    /* 初始白色背景*/
+    length = SQUARE_LENGTH * NUM_OF_BLOCK + SQUARE_GAP * (NUM_OF_BLOCK + 1);
+    stx = WIDTH / 2 - length / 2;
+    sty = HEIGHT / 2 - length / 2;
+    setfillcolor(WHITE);
+    drawSquare(stx, sty, length);
+
+    /* 计算各区块的起始位置 */
+    for (i = 0; i < NUM_OF_BLOCK; i++)
+    {
+        /* x */
+        blockPos[i][0] = stx + SQUARE_GAP * (i + 1) + SQUARE_LENGTH * i;
+        /* y */
+        blockPos[i][1] = sty + SQUARE_GAP * (i + 1) + SQUARE_LENGTH * i;
+    }
+
+    for (i = 0; i < NUM_OF_BLOCK; i++)
+    {
+        for (j = 0; j < NUM_OF_BLOCK; j++)
+        {
+            blockRect[i][j].left = blockPos[j][0];
+            blockRect[i][j].top = blockPos[i][1];
+            blockRect[i][j].right = blockPos[j][0] + SQUARE_LENGTH;
+            blockRect[i][j].bottom = blockPos[i][1] + SQUARE_LENGTH;
+            pEmptyBlock[i * NUM_OF_BLOCK + j] = &blockVal[i][j];
+        }
+    }
+
+    emptyBlock = NUM_OF_BLOCK * NUM_OF_BLOCK;
+    createTwoNewBlock();
+}
+
+void gameClose(void)
+{
+    closegraph();
+}
+
+int main()
+{
+    gameInit();
+    gameStart();
+    _getch();
+    gameClose();
+    return 0;
+}
